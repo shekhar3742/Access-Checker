71096ebf94874546a74398435d01d76e015da59a
shekhar3742 <imderek3742@gmail.com>
2026-03-13 10:16:54 +0530

    first code

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import CheckIn from './Components/LocationChecker'
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+    <CheckIn/>
+      
+      
+    </>
+  )
+}
+
+export default App
